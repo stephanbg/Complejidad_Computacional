b3df0a5460bd9df1aac54d94a95e550b4bd33779
Stephan Brommer Gutiérrez <alu0101493497@ull.edu.es>
2024-12-15 18:19:17 +0000

    Update clause.h

--- p04/src/SAT/clauses/clause.h
+++ p04/src/SAT/clauses/clause.h
@@ -1,3 +1,23 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Complejidad Computacional
+ * Curso: 4º
+ * Práctica 4: Resolver algoritmo SAT
+ *
+ * @author Aday Cuesta Correa
+ * @author Stephan Brommer Gutiérrez
+ * @author Sofía De Fuentes Rosella
+ * @since 1 de Diciembre de 2024
+ * @file clause.h
+ * @brief Definición de la clase Clause, que representa una cláusula en un 
+ *        problema de satisfacibilidad (SAT). Una cláusula es una disyunción de 
+ *        variables booleanas, cada una de las cuales puede estar negada o no.
+ * 
+ * @see {@link https://github.com/stephanbg/Complejidad_Computacional/edit/main/p04}
+ */
+
 #pragma once
 
 #include <vector>
@@ -11,9 +31,7 @@ class Clause {
  public:
   Clause() : variables_() {}
   Clause(const nlohmann::json& json);
-
   std::vector<Variable> GetVariables() const { return variables_; }
-
   friend std::ostream& operator<<(std::ostream& output, const Clause& clause);
  private:
   std::vector<Variable> variables_;
